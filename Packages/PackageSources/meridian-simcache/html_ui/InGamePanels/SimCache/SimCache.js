155566d95d17c84e88b01eaf5ab8be740249615e
Orion Lyau <ollyau@users.noreply.github.com>
2024-03-28 19:27:31 -0700

    Add preliminary tracking UI to InGamePanel (#8)

--- Packages/PackageSources/meridian-simcache/html_ui/InGamePanels/SimCache/SimCache.js
+++ Packages/PackageSources/meridian-simcache/html_ui/InGamePanels/SimCache/SimCache.js
@@ -9,34 +9,88 @@ class SimCachePanel extends UIElement {
     connectedCallback() {
         this.m_connected = true;
 
-        this.m_ingameUI = this.querySelector( "ingame-ui" );
-        this.m_ingameUI.addEventListener( "panelActive", () => this.onPanelActive() );
+        this.m_ingameUI = this.querySelector("ingame-ui");
+        this.m_ingameUI.addEventListener("panelActive", () => this.onPanelActive());
+
+        this.m_titleElement = document.getElementById("cache-title");
+        this.m_subtitleElement = document.getElementById("cache-subtitle");
+        this.m_annulusElement = document.getElementById("inner-annulus");
 
         let updateLoop = () => {
-            if ( !this.m_connected ) {
+            if (!this.m_connected) {
                 return;
             }
 
             try {
                 this.UpdatePanel();
             }
-            catch ( Error ) {
-                console.error( document.title + " : " + Error );
+            catch (Error) {
+                console.error(document.title + " : " + Error);
             }
-            requestAnimationFrame( updateLoop );
+            requestAnimationFrame(updateLoop);
         };
-        requestAnimationFrame( updateLoop );
+        requestAnimationFrame(updateLoop);
+    }
+
+    updateCacheTitle(title) {
+        this.m_titleElement.innerText = title;
+    }
+
+    updateRange(distanceMeters) {
+        const piecewiseRange = this.getPiecewiseRange(distanceMeters);
+        if (this.m_currentPiecewiseRange !== piecewiseRange) {
+            this.m_currentPiecewiseRange = piecewiseRange;
+
+            // entered a new annulus; update things as necessary
+            this.m_subtitleElement.innerText = this.getSubtitle(piecewiseRange);
+            this.m_annulusElement.setAttributeNS(null, "r", this.getAnnulusInnerRadius(piecewiseRange));
+        }
+    }
+
+    getSubtitle(piecewiseRange) {
+        return piecewiseRange !== null ? `Less than ${piecewiseRange} nm away` : "Out of range";
+    }
+
+    getPiecewiseRange(distanceMeters) {
+        if (distanceMeters < 0) {
+            throw new RangeError("Negative distances are not allowed");
+        }
+        if (distanceMeters < 3704) {
+            return 2;
+        }
+        if (distanceMeters < 9260) {
+            return 5;
+        }
+        if (distanceMeters < 18520) {
+            return 10;
+        }
+        if (distanceMeters < 46300) {
+            return 25;
+        }
+        return null;
+    }
+
+    getAnnulusInnerRadius(piecewiseRange) {
+        switch (piecewiseRange) {
+            case 2:
+                return 0;
+            case 5:
+                return 22;
+            case 10:
+                return 31;
+            case 25:
+                return 40;
+            default:
+                return 49;
+        }
     }
 
     onPanelActive() {
-        // TODO: add initialization on panel active
+        this.updateCacheTitle("Cache Title Placeholder");
+        this.updateRange(100000);
     }
 
     UpdatePanel() {
-        if ( !SimVar.IsReady() ) {
-            return;
-        }
-
         // TODO: add items to repeat within update loop
     }
 
@@ -44,7 +98,7 @@ class SimCachePanel extends UIElement {
 
 //-----------------------------------------------------------------------------
 
-window.customElements.define( "ingamepanel-simcache", SimCachePanel);
+window.customElements.define("ingamepanel-simcache", SimCachePanel);
 checkAutoload();
 
 //-----------------------------------------------------------------------------
