17c9518f4316e5602779aee1967a0677777a67c1
Steven Frost <steve.frost.private@gmail.com>
2024-03-30 13:21:31 +0000

    Improved parameter naming for Utils::HasFlag

--- Source/Utils/Inc/Utils/Enum/EnumUtils.h
+++ Source/Utils/Inc/Utils/Enum/EnumUtils.h
@@ -12,12 +12,12 @@ namespace Utils
 // -----------------------------------------------------------------------------
 
 template< class TEnum >
-bool HasFlag( const TEnum Flags, const TEnum Flag )
+bool HasFlag( const TEnum Value, const TEnum FlagToCheck )
 {
 	static_assert( std::is_enum< TEnum >::value, "TEnum must be an enum type" );
 
 	typedef typename std::underlying_type< TEnum >::type TEnumType;
-	return ( static_cast< TEnumType >( Flags ) & static_cast< TEnumType >( Flag ) ) == static_cast< TEnumType >( Flag );
+	return ( static_cast< TEnumType >( Value ) & static_cast< TEnumType >( FlagToCheck ) ) == static_cast< TEnumType >( FlagToCheck );
 }
 
 // -----------------------------------------------------------------------------
