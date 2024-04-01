dd48270997351f81a86d19dadefea88576314344
Steven Frost <steve.frost.private@gmail.com>
2024-04-01 23:18:55 +0100

    Added macro to allow logging to be disabled

--- Source/Utils/Inc/Utils/Logging/Log.h
+++ Source/Utils/Inc/Utils/Logging/Log.h
@@ -7,10 +7,21 @@
 
 // -----------------------------------------------------------------------------
 
+#if LOGGING_ENABLED
 #define LOG( CategoryName, CategoryLevel, Message, ... ) g_traceArea##CategoryName.Output( Utils::Logging::Level::CategoryLevel, Message, ##__VA_ARGS__ );
 
 #define DECLARE_LOG_CATEGORY( CategoryName ) extern Utils::Logging::Category g_traceArea##CategoryName;
 #define DEFINE_LOG_CATEGORY( CategoryName, CategoryLevel ) Utils::Logging::Category g_traceArea##CategoryName( #CategoryName, Utils::Logging::Level::CategoryLevel );
+#else
+#define LOG( CategoryName, CategoryLevel, Message, ... )
+
+#define DECLARE_LOG_CATEGORY( CategoryName )
+#define DEFINE_LOG_CATEGORY( CategoryName, CategoryLevel )
+#endif // LOGGING_ENABLED
+
+// -----------------------------------------------------------------------------
+
+#if LOGGING_ENABLED
 
 // -----------------------------------------------------------------------------
 
@@ -57,6 +68,10 @@ private:
 
 } // namespace Logging
 } // namespace Utils
+
+#endif // LOGGING_ENABLED
+
+// -----------------------------------------------------------------------------
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
