700b5a04473852300e18dea5aed850a88302c30e
Steven Frost <steve.frost.private@gmail.com>
2024-04-02 19:08:30 +0100

    Script for copying built packages to the MSFS community folder

---
+++ Scripts/CopyBuiltPackagesToCommunityFolder.bat
@@ -0,0 +1,37 @@
+@echo off
+
+set StoreUserConfigLocation=%LOCALAPPDATA%\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\UserCfg.opt
+set SteamUserConfigLocation=%APPDATA%\Microsoft Flight Simulator\UserCfg.opt
+
+if exist "%StoreUserConfigLocation%" (
+	set UserConfigFileLocation=%StoreUserConfigLocation%
+	goto CopyPackage
+) else (
+	if exist "%SteamUserConfigLocation%" (	
+		set UserConfigFileLocation=%SteamUserConfigLocation%
+		goto CopyPackage
+	) else (
+		echo "Failed to locate UserCfg.opt"
+		goto Exit
+	)
+)
+
+
+:CopyPackage
+for /f "tokens=1,*" %%a IN ( 'findstr /C:"InstalledPackagesPath" %UserConfigFileLocation%' ) do (
+    set PackagesDirectory=%%b
+)
+set PackagesDirectory=%PackagesDirectory:"=%
+set CommunityDirectory=%PackagesDirectory%\Community
+
+cd ..\Packages\
+
+set SourcePackageDirectory=%cd%\Built\Packages\meridian-simcache
+set DestinationPackageDirectory=%CommunityDirectory%\meridian-simcache
+
+if exist "%SourcePackageDirectory%" (
+	rmdir /s /q "%DestinationPackageDirectory%"
+	robocopy "%SourcePackageDirectory%" "%DestinationPackageDirectory%" /E
+)
+
+:Exit
