771800c7eca05dd1cfff20e585d7680bcce5f6de
Steven Frost <steve.frost.private@gmail.com>
2024-04-09 21:27:39 +0100

    DateTime utility class (#20)
    * DateTime utility class
    
    * Added an option to disable milliseconds output when converting a DateTime to a string. Improved date time string to DateTime input validation.
    
    * Input validation improvements
    
    * Switching string size() calls to empty()

---
+++ Source/Utils/Inc/Utils/Time/DateTime.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2024 Steven Frost and Orion Lyau
+
+#pragma once
+
+#include <chrono>
+#include <string>
+
+// -----------------------------------------------------------------------------
+
+namespace Utils
+{
+
+// -----------------------------------------------------------------------------
+
+class DateTime
+{
+public:
+
+	DateTime();
+	explicit DateTime( const std::chrono::system_clock::time_point& TimePoint );
+
+	std::string ToISO8601UTC( const bool IncludeMilliseconds = false ) const;
+
+	static DateTime Now();
+	static DateTime FromISO8601UTC( const std::string& DateTimeString );
+
+private:
+
+	std::chrono::system_clock::time_point TimePoint;
+};
+
+// -----------------------------------------------------------------------------
+
+} // namespace Utils
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
---
+++ Source/Utils/Source/Time/DateTime.cpp
@@ -0,0 +1,112 @@
+// Copyright (c) 2024 Steven Frost and Orion Lyau
+
+#include <Utils/Time/DateTime.h>
+
+#include <Utils/Logging/Log.h>
+
+#include <iomanip>
+#include <sstream>
+
+// -----------------------------------------------------------------------------
+
+DEFINE_LOG_CATEGORY( DateTime, Info )
+
+// -----------------------------------------------------------------------------
+
+namespace Utils
+{
+
+// -----------------------------------------------------------------------------
+
+namespace DateTimePrivate
+{
+	static constexpr char* ISO8601Format = "%FT%T";
+} // namespace DateTimePrivate
+
+// -----------------------------------------------------------------------------
+
+DateTime::DateTime()
+	: TimePoint()
+{
+}
+
+// -----------------------------------------------------------------------------
+
+DateTime::DateTime( const std::chrono::system_clock::time_point& TimePoint )
+	: TimePoint( TimePoint )
+{
+}
+
+// -----------------------------------------------------------------------------
+
+std::string DateTime::ToISO8601UTC( const bool IncludeMilliseconds ) const
+{
+	const auto SecondsSinceEpoch = std::chrono::system_clock::to_time_t( TimePoint );
+	const auto* TimeComponents = gmtime( &SecondsSinceEpoch );
+
+	std::stringstream StringStream;
+	StringStream << std::put_time( TimeComponents, DateTimePrivate::ISO8601Format );
+
+	if ( IncludeMilliseconds )
+	{
+		const auto TruncatedTime = std::chrono::system_clock::from_time_t( SecondsSinceEpoch );
+		const auto Milliseconds = std::chrono::duration_cast< std::chrono::milliseconds >( TimePoint - TruncatedTime ).count();
+
+		StringStream << "." << std::fixed << std::setw( 3 ) << std::setfill( '0' ) << Milliseconds;
+	}
+
+	StringStream << "Z";
+
+	return StringStream.str();
+}
+
+// -----------------------------------------------------------------------------
+
+DateTime DateTime::Now()
+{
+	return DateTime( std::chrono::system_clock::now() );
+}
+
+// -----------------------------------------------------------------------------
+
+DateTime DateTime::FromISO8601UTC( const std::string& DateTimeString )
+{
+	if ( DateTimeString.empty() )
+	{
+		LOG( DateTime, Error, "FromISO8601UTC called with an empty string" );
+		return DateTime();
+	}
+
+	if ( DateTimeString[ DateTimeString.size() - 1 ] != 'Z' )
+	{
+		LOG( DateTime, Error, "FromISO8601UTC called with a non-UTC date time string" );
+		return DateTime();
+	}
+
+	std::istringstream StringStream( DateTimeString );
+	std::tm TimeComponents = {};
+
+	std::string Tail;
+	StringStream >> std::get_time( &TimeComponents, DateTimePrivate::ISO8601Format ) >> Tail;
+
+	const auto SecondsSinceEpoch = std::mktime( &TimeComponents );
+	auto TimePoint = std::chrono::system_clock::from_time_t( SecondsSinceEpoch );
+
+	if ( !Tail.empty() && Tail[ 0 ] == '.' )
+	{
+		const auto MillisecondsString = Tail.substr( 1, Tail.size() - 2 );
+		const auto Milliseconds = std::atoi( MillisecondsString.c_str() );
+
+		TimePoint += std::chrono::milliseconds( Milliseconds );
+	}
+
+	return DateTime( TimePoint );
+}
+
+// -----------------------------------------------------------------------------
+
+} // namespace Utils
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
