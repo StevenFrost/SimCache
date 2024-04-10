9939b455ee38618f479425bde43404fe549624f2
Steven Frost <steve.frost.private@gmail.com>
2024-04-10 13:05:01 +0100

    Fixes and improvements to build packaging and deployment scripts (#21)
    * Updated the BuildAllPackages script to behave consistently for Steam & Store versions of MSFS
    
    * Improved directory removal handling for the copy built packages to community folder script

--- Scripts/BuildAllPackages.bat
+++ Scripts/BuildAllPackages.bat
@@ -1,4 +1,36 @@
 @echo off
 
+set PackageToolLocation=%MSFS_SDK%Tools\bin\fspackagetool.exe
+set StoreUserConfigLocation=%LOCALAPPDATA%\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\UserCfg.opt
+set SteamUserConfigLocation=%APPDATA%\Microsoft Flight Simulator\UserCfg.opt
+
 cd ..\Packages\
-start "" "%MSFS_SDK%Tools\bin\fspackagetool.exe" ".\SimCache.xml" -nopause
+
+if not exist "%PackageToolLocation%" (
+    echo "Unable to locate fspackagetool.exe"
+    goto Exit
+)
+
+if exist "%StoreUserConfigLocation%" (
+	goto Store
+) else (
+	if exist "%SteamUserConfigLocation%" (	
+		goto Steam
+	) else (
+		echo "Failed to locate UserCfg.opt"
+		goto Exit
+	)
+)
+
+
+:Store
+start "" "%PackageToolLocation%" ".\SimCache.xml" -nopause
+goto Exit
+
+
+:Steam
+start "" "%PackageToolLocation%" ".\SimCache.xml" -forcesteam
+goto Exit
+
+
+:Exit
