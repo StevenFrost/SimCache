e128f8663502d4d2f6f849d97a4539a7fdc8f73b
Orion <orion@domain.tld>
2024-04-18 23:33:50 -0700

    Initial implementation of Ellipsoid and EarthCoordinate

---
+++ Source/Utils/Inc/Utils/Geodesy/EarthCoordinate.h
@@ -0,0 +1,49 @@
+// Copyright (c) 2024 Steven Frost and Orion Lyau
+
+#pragma once
+
+#include <Utils/Math/Vector3.h>
+
+// -----------------------------------------------------------------------------
+
+namespace Utils
+{
+
+// -----------------------------------------------------------------------------
+
+class EarthCoordinate
+{
+private:
+
+	EarthCoordinate( const Vector3& GeocentricPosition );
+
+public:
+
+	static EarthCoordinate FromGeodetic( const double LatitudeDegrees, const double LongitudeDegrees, const double HeightAboveEllipsoidFeet );
+
+	EarthCoordinate( const EarthCoordinate& Other ) = default;
+	EarthCoordinate( EarthCoordinate&& Other ) = default;
+
+	EarthCoordinate& operator=( const EarthCoordinate& Other ) = default;
+	EarthCoordinate& operator=( EarthCoordinate&& Other ) = default;
+
+	bool operator==( const EarthCoordinate& Other ) const;
+	bool operator!=( const EarthCoordinate& Other ) const;
+
+	EarthCoordinate operator+( const Vector3& RHS ) const;
+	Vector3 operator-( const EarthCoordinate& RHS ) const;
+
+	double DistanceTo( const EarthCoordinate& Other ) const;
+
+public:
+
+	Vector3 Geocentric;
+};
+
+// -----------------------------------------------------------------------------
+
+} // namespace Utils
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
